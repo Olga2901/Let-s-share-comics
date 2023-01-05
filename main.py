4ff8a013004cf245cedced599ce2b27837466d3b
Olga2901 <104138981+Olga2901@users.noreply.github.com>
2023-01-05 20:22:47 +0300

    Сorrected the names of the environment variables

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ def get_random_url_xkcd():
     return response["alt"]
 
 
-def get_address_for_upload_img(access_token, group_id, user_id):
+def get_address_for_upload_img(vk_token, vk_group_id, vk_user_id):
     photos_wall_url = "https://api.vk.com/method/photos.getWallUploadServer"
     params = {
-        "access_token": access_token,
+        "access_token": vk_token,
         "v": "5.131",
-        "user_id": user_id,
-        "group_id": group_id,
+        "user_id": vk_user_id,
+        "group_id": vk_group_id,
     }
     response = requests.get(photos_wall_url, params=params)
     response.raise_for_status()
@@ -39,13 +39,13 @@ def upload_img_to_server(upload_url):
     return upload_url_response["server"], upload_url_response["photo"], upload_url_response["hash"] 
 
 
-def save_img_to_vk(access_token, user_id, group_id, server, photo_url, hash):
+def save_img_to_vk(vk_token, vk_user_id, vk_group_id, server, photo_url, hash):
     photos_save_url = "https://api.vk.com/method/photos.saveWallPhoto"
     params = {
-        "access_token": access_token,
+        "access_token": vk_token,
         "v": "5.131",
-        "user_id": user_id,
-        "group_id": group_id,
+        "user_id": vk_user_id,
+        "group_id": vk_group_id,
         "photo": photo_url,
         "server": server,
         "hash": hash,
@@ -56,12 +56,12 @@ def save_img_to_vk(access_token, user_id, group_id, server, photo_url, hash):
     return save_photos_response["response"][0]["owner_id"], save_photos_response["response"][0]["id"]
 
 
-def make_wall_post_vk(access_token, group_id, owner_id, photo_id, comic_commentary):
+def make_wall_post_vk(vk_token, vk_group_id, owner_id, photo_id, comic_commentary):
     wall_post_url = "https://api.vk.com/method/wall.post"
     params = {
-        "access_token": access_token,
+        "access_token": vk_token,
         "v": "5.131",
-        "owner_id": f"-{group_id}",
+        "owner_id": f"-{vk_group_id}",
         "from_group": 1,
         "message": comic_commentary, 
         "attachments": f"photo{owner_id}_{photo_id}",
@@ -72,16 +72,16 @@ def make_wall_post_vk(access_token, group_id, owner_id, photo_id, comic_commenta
 
 if __name__ == "__main__":
     load_dotenv()
-    access_token = os.getenv("ACCESS_TOKEN")
-    user_id = os.getenv("USER_ID")
-    group_id = os.getenv("GROUP_ID")
+    vk_token = os.getenv("VK_TOKEN")
+    vk_user_id = os.getenv("VK_USER_ID")
+    vk_group_id = os.getenv("VK_GROUP_ID")
     get_random_url_xkcd()
     comic_commentary = get_random_url_xkcd()
-    get_address_for_upload_img(access_token, group_id, user_id)
-    upload_url = get_address_for_upload_img(access_token, group_id, user_id)
+    get_address_for_upload_img(vk_token, vk_group_id, vk_user_id)
+    upload_url = get_address_for_upload_img(vk_token, vk_group_id, vk_user_id)
     upload_img_to_server(upload_url)
     server, photo_url, hash = upload_img_to_server(upload_url)
-    save_img_to_vk(access_token, user_id, group_id, server, photo_url, hash)
-    owner_id, photo_id = save_img_to_vk(access_token, user_id, group_id, server, photo_url, hash)
-    make_wall_post_vk(access_token, group_id, owner_id, photo_id, comic_commentary)
+    save_img_to_vk(vk_token, vk_user_id, vk_group_id, server, photo_url, hash)
+    owner_id, photo_id = save_img_to_vk(vk_token, vk_user_id, vk_group_id, server, photo_url, hash)
+    make_wall_post_vk(vk_token, vk_group_id, owner_id, photo_id, comic_commentary)
     os.remove("image.png")
